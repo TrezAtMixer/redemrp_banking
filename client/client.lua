b9cbc6d19f0ee749d03c5eccedfb31ff64eb02e7
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-01-08 21:44:48 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -5,57 +5,6 @@ RegisterCommand('bank', function(source)
 			SendNUIMessage({type = 'openGeneral'})
 			TriggerServerEvent('redemrp_banking:balance2')
 end)
-
-RegisterCommand('closestPlayer', function(source)
-	local closestPlayer, closestDistance = GetClosestPlayer()
-	print(closestPlayer)
-	print(closestDistance)
-		
-end)
-
-GetPlayers = function()
-	local players = {}
-
-	for _,player in ipairs(GetActivePlayers()) do
-		local ped = GetPlayerPed(player)
-
-		if DoesEntityExist(ped) then
-			table.insert(players, player)
-		end
-	end
-
-	return players
-end
-
-GetClosestPlayer = function(coords)
-	local players, closestDistance, closestPlayer = GetPlayers(), -1, -1
-	local coords, usePlayerPed = coords, false
-	local playerPed, playerId = PlayerPedId(), PlayerId()
-
-	if coords then
-		coords = vector3(coords.x, coords.y, coords.z)
-	else
-		usePlayerPed = true
-		coords = GetEntityCoords(playerPed)
-	end
-
-	for i=1, #players, 1 do
-		local target = GetPlayerPed(players[i])
-
-		if not usePlayerPed or (usePlayerPed and players[i] ~= playerId) then
-			local targetCoords = GetEntityCoords(target)
-			local distance = #(coords - targetCoords)
-
-			if closestDistance == -1 or closestDistance > distance then
-				closestPlayer = players[i]
-				closestDistance = distance
-			end
-		end
-	end
-
-	return closestPlayer, closestDistance
-end
-
 --=================Deposit Event===================
 local l_ 
 local f_ 
